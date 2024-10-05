7f5fa339219cf44c43d85138ce845c303f703865
CAndresSierra <camilofullstack@gmail.com>
2024-10-05 15:37:42 -0500

    updated: good practices implemented

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,13 @@
 import server from "./server";
 import { PORT } from "./config/envs";
 import dbConfig from "./config/dbConfig";
-import {
-  preloadCharacters,
-  preloadPlanets,
-  preloadTransformations,
-} from "./utils/preloadDta";
+import * as seedDta from "./utils/preloadDta";
 
 const InitializeApp = async () => {
   await dbConfig();
-  await preloadCharacters();
-  await preloadPlanets();
-  await preloadTransformations();
+  await seedDta.preloadCharacters();
+  await seedDta.preloadPlanets();
+  await seedDta.preloadTransformations();
   server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
   });
